d96f8f428fcd0f6964bd28af0b7982ded53ed2af
volkandmbs <51151583+volkandmbs@users.noreply.github.com>
2019-06-03 09:27:09 +0200

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Ranked
+            Type = GradeBookType.Ranked;
         }
     }
 }
